c09d933bf555fea6210a4a3e9aef41f707178f3e
Vierling, Marvin <marvinvierling@hotmail.de>
2017-05-15 19:23:43 +0200

    args for path delete useless attributes

--- OpenStreetMap/Program.cs
+++ OpenStreetMap/Program.cs
@@ -1,15 +1,12 @@
 ﻿using System;
-using System.Collections.Generic;
 
 namespace OpenStreetMap
 {
     class Program
     {
-        static Dictionary<long, Node> myNode = new Dictionary<long, Node>();
-        static List<Edge> myWays = new List<Edge>();
         static void Main(string[] args)
         {
-            Map map = Map.MapLoader(@"map");
+            Map map = Map.MapLoader(args[0]);
             Console.ReadLine();
         }
     }
